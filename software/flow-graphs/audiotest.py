78b6fa0fca6fc193810c00a0dac22f3a38ffcba8
Tom M0LTE <tom@m0lte.uk>
2022-01-04 23:05:24 +0000

    Add audio recording test flowgraph

---
+++ software/flow-graphs/audiotest.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: tf
+# GNU Radio version: 3.8.1.0
+
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+
+class audiotest(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.audio_source_0 = audio.source(samp_rate, '', True)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.audio_source_0, 0), (self.blocks_null_sink_0, 0))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+
+
+def main(top_block_cls=audiotest, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
