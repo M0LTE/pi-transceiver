cf7d5e73e5489981e4120c7f02d325f08da6b048
Tom M0LTE <tom@m0lte.uk>
2022-01-04 22:27:57 +0000

    Add DC block and LPF to flow graph, make it runnable from the command line

--- software/flow-graphs/SSB_TX.py
+++ software/flow-graphs/SSB_TX.py
@@ -8,63 +8,21 @@
 # Title: Not titled yet
 # GNU Radio version: 3.8.1.0
 
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
 from gnuradio import audio
 from gnuradio import filter
-from gnuradio import gr
 from gnuradio.filter import firdes
+from gnuradio import gr
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import limesdr
-from gnuradio import qtgui
 
-class SSB_TX(gr.top_block, Qt.QWidget):
+class SSB_TX(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "SSB_TX")
-
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
 
         ##################################################
         # Variables
@@ -97,6 +55,17 @@ class SSB_TX(gr.top_block, Qt.QWidget):
 
         self.limesdr_sink_0.calibrate(2.5e6, 0)
         self.hilbert_fc_0 = filter.hilbert_fc(65, firdes.WIN_HAMMING, 6.76)
+        self.dc_blocker_xx_1 = filter.dc_blocker_ff(32, True)
+        self.band_pass_filter_0 = filter.fir_filter_fff(
+            1,
+            firdes.band_pass(
+                1,
+                samp_rate,
+                200,
+                2800,
+                10,
+                firdes.WIN_HAMMING,
+                6.76))
         self.audio_source_0 = audio.source(samp_rate, '', True)
 
 
@@ -104,19 +73,17 @@ class SSB_TX(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.audio_source_0, 0), (self.hilbert_fc_0, 0))
+        self.connect((self.audio_source_0, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.dc_blocker_xx_1, 0))
+        self.connect((self.dc_blocker_xx_1, 0), (self.hilbert_fc_0, 0))
         self.connect((self.hilbert_fc_0, 0), (self.limesdr_sink_0, 0))
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "SSB_TX")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 200, 2800, 10, firdes.WIN_HAMMING, 6.76))
         self.limesdr_sink_0.set_digital_filter(self.samp_rate, 0)
         self.limesdr_sink_0.set_digital_filter(self.samp_rate, 1)
 
@@ -129,31 +96,18 @@ class SSB_TX(gr.top_block, Qt.QWidget):
 
 
 def main(top_block_cls=SSB_TX, options=None):
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-    tb.start()
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
-        Qt.QApplication.quit()
+        tb.stop()
+        tb.wait()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.aboutToQuit.connect(quitting)
-    qapp.exec_()
+    tb.start()
+    tb.wait()
 
 
 if __name__ == '__main__':
